828a7069a13268d09ebea4d82d8c02cfb11ad96e
bfh687 <57474145+bfh687@users.noreply.github.com>
2022-01-06 17:44:44 -0800

    adds animation labels

--- entities/stormhead.js
+++ entities/stormhead.js
@@ -2,21 +2,27 @@ class StormHead {
     constructor(game) {
         Object.assign(this, { game });
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/stormhead.png");
-
         this.animations = [];
         this.loadAnimations();
     }
 
     loadAnimations() {
+        // idle animation
         this.animations.push(
             new Animator(this.spritesheet, 0, 0, 192, 192, 9, 0.16, 0, false, true)
         );
+
+        // walking animation
         this.animations.push(
             new Animator(this.spritesheet, 0, 192, 192, 192, 10, 0.16, 0, false, true)
         );
+
+        // attack animation
         this.animations.push(
             new Animator(this.spritesheet, 0, 384, 192, 192, 21, 0.16, 0, false, true)
         );
+
+        // death animation
         this.animations.push(
             new Animator(this.spritesheet, 0, 768, 192, 192, 7, 0.16, 0, false, true)
         );
@@ -25,6 +31,7 @@ class StormHead {
     update() {}
 
     draw(ctx) {
+        // idle animation
         this.animations[0].drawFrame(
             this.game.clockTick,
             ctx,
@@ -32,6 +39,8 @@ class StormHead {
             ctx.canvas.height / 2 - 192 / 2 - 40,
             1
         );
+
+        // walking animation
         this.animations[1].drawFrame(
             this.game.clockTick,
             ctx,
@@ -39,6 +48,8 @@ class StormHead {
             ctx.canvas.height / 2 - 192 / 2 - 40,
             1
         );
+
+        // attack animation
         this.animations[2].drawFrame(
             this.game.clockTick,
             ctx,
@@ -46,6 +57,8 @@ class StormHead {
             ctx.canvas.height / 2 - 192 / 2 - 40,
             1
         );
+
+        // death animation
         this.animations[3].drawFrame(
             this.game.clockTick,
             ctx,
